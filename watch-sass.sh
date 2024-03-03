e5d0f727fe665de7f1a1c3e72b24b64ee341d620
datsfilipe <datsfilipe.foss@proton.me>
2024-03-03 00:09:11 -0300

    refactor: update development workflow and and bash script
    * the old development workflow was tied to better discord, now it's tied
      to [Vencord](https://github.com/Vendicated/Vencord)

---
+++ watch-sass.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+yarn sass --watch src/scss:output --style=compressed &
+
+compile_and_rename() {
+  yarn sass src/scss:/tmp --style=compressed
+
+  mv /tmp/custom.css $HOME/.config/Vencord/settings/quickCss.css
+}
+
+compile_and_rename
+while true; do
+  inotifywait -r -e modify,create,delete src/scss
+  compile_and_rename
+done
